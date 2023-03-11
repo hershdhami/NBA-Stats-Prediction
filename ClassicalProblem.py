9af38c98f06c57dd764448c2cc45680ce9f1fdab
Hersh Dhami <69739606+hershdhami@users.noreply.github.com>
2023-03-11 18:31:28 -0500

    Added functionality for NBA ML, underfitted by a bit though!

--- ClassicalProblem.py
+++ ClassicalProblem.py
@@ -25,16 +25,13 @@ X.shape, y.shape
 X = torch.from_numpy(X).type(torch.float)
 y = torch.from_numpy(y).type(torch.float)
 
-#View the first five samples
-X[:5], y[:5]
-
 X_train, X_test, y_train,y_test = train_test_split( X,
                                                     y,
                                                     test_size=0.2,
                                                     random_state=42)
 
 #In order to activate this we need to enable GPU access VIA plugin
-device = "cude" if torch.cuda.is_available() else "cpu"
+#device = "cude" if torch.cuda.is_available() else "cpu"
 
 #We are going to implement supervised learning because we have features and labels and we are trying to link them up
 
@@ -47,13 +44,13 @@ class CircleModelV0(nn.Module):
     def forward(self, x:torch.Tensor) -> torch.Tensor:
         return self.layer_2(self.layer_1(x)) # computation goes through layer 1 and then layer 2
     
-model_0 = CircleModelV0().to(device)
+model_0 = CircleModelV0()
 
 #We can also do the same thing by using nn.Sequential
 model_1 = nn.Sequential(
     nn.Linear(in_features=2, out_features=5),
     nn.Linear(in_features=5, out_features=1)
-).to(device)
+)
 
 model_1
 
@@ -67,12 +64,69 @@ print(f"The first 10 test results were: {y_test[:10]} \n")
 
 #Need to use a different loss function because the one we originally chose ONLY works on regression problems
 
-loss_fn = nn.BCEWithLogitsLoss()
-
-optimizer = torch.optim.SGD(params=model_0.parameters(),
-                            lr=0.1)
-
-def accuracy(y_true, y_pred):
+def accuracy_fn(y_true, y_pred):
     ratio = torch.eq(y_true,y_pred).sum().item()
     totalRatio = ratio/len(y_pred) * 100
     return totalRatio
+
+#This model will not work because we are using only lines to predict a circular graph
+    #We are underfitting the model meaning we need to learn predictive patterns from data
+    #We are going to need to create a new entire model!
+
+
+class CircleModelV2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer_1 = nn.Linear(in_features=2, out_features=10)
+        self.layer_2 = nn.Linear(in_features=10, out_features=10)
+        self.layer_3 = nn.Linear(in_features=10, out_features=1)
+        self.relu = nn.ReLU() # <- add in ReLU activation function
+        # Can also put sigmoid in the model 
+        # This would mean you don't need to use it on the predictions
+        # self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+      # Intersperse the ReLU activation function between layers
+       return self.layer_3(self.relu(self.layer_2(self.relu(self.layer_1(x)))))
+    
+model_2 = CircleModelV2()
+
+loss_fn = nn.BCEWithLogitsLoss()
+
+optimizer = torch.optim.SGD(params=model_2.parameters(),
+                            lr=0.1)
+
+torch.manual_seed(42)
+epochs = 1000
+
+for epoch in range(epochs):
+    #A logit is a function that represents probability from 0 to 1
+    y_logits = model_2(X_train).squeeze()
+    y_pred = torch.round(torch.sigmoid(y_logits))
+
+    loss = loss_fn(y_logits, y_train)
+    acc = accuracy_fn(y_true=y_train,y_pred=y_pred)
+
+    optimizer.zero_grad()
+
+    loss.backward()
+
+    optimizer.step()
+
+    model_2.eval()
+    with torch.inference_mode():
+        # 1. Forward Pass
+        test_logits = model_2(X_test).squeeze()
+        test_pred = torch.round(torch.sigmoid(test_logits))
+        # 2. Calculate loss and accuracy
+        test_loss = loss_fn(test_logits,y_test)
+        test_acc = accuracy_fn(y_true=y_test,y_pred=test_pred)
+
+    if epoch % 100 == 0:
+        print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test Accuracy: {test_acc:.2f}%")
+
+model_2.eval()
+with torch.inference_mode():
+    y_preds = torch.round(torch.sigmoid(model_2(X_test).squeeze()))
+
+y_preds[:10], y_test[:10]
--- NBAStats.py
+++ NBAStats.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import torch
+from torch import nn
 import matplotlib.pyplot as plt
 
 #IN FUTURE Web Scrape the NBA data and find out which player has been increasingly doing good
@@ -8,8 +9,14 @@ import matplotlib.pyplot as plt
 df = pd.read_excel("NBAStats.xlsx", )
 print(df)
 
-nbaStatline = torch.from_numpy(df.to_numpy()[:,:8])
-nbaLabel = torch.from_numpy(df.to_numpy()[:,8:])
+nbaStatline = torch.from_numpy(df.to_numpy()[:,:8]).type(torch.float)
+nbaLabel = torch.from_numpy(df.to_numpy()[:,8:]).type(torch.float)
+
+nbaStatLineTrain = nbaStatline[:5,:]
+nbaStatLineTest = nbaStatline[5:,:]
+
+nbaLabelTrain = nbaLabel[:5].squeeze()
+nbaLabelTest = nbaLabel[5:].squeeze()
 
 
 #This displays the graphs compares to Good/Bad
@@ -31,3 +38,69 @@ def plotAllFeatures(feature1,
 
 plotAllFeatures(nbaStatline[:,0], nbaStatline[:,1], nbaStatline[:,2], nbaLabel)
 
+class nbaPlayerDefineModel(nn.Module):
+    """Attempting to predict whether NBA Player did Good/Bad in a certain night"""
+    def __init__(self):
+        super().__init__()
+        self.layer_1 = nn.Linear(in_features=8, out_features=10)
+        self.layer_2 = nn.Linear(in_features=10, out_features=10)
+        self.layer_3 = nn.Linear(in_features=10, out_features=1)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.layer_3(self.layer_2(self.relu(self.layer_1(x))))
+
+def accuracy_fn(y_true, y_pred):
+    ratio = torch.eq(y_true,y_pred).sum().item()
+    totalRatio = ratio/len(y_pred) * 100
+    return totalRatio
+
+model_0 = nbaPlayerDefineModel()
+
+torch.manual_seed(42)
+epochs = 1000
+
+loss_fn = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.SGD(params=model_0.parameters(),
+                            lr=0.1)
+
+print()
+
+for epoch in range(epochs):
+    model_0.train()
+
+    y_logits = model_0(nbaStatLineTrain).squeeze()
+    y_pred = torch.round(torch.sigmoid(y_logits))
+
+    loss = loss_fn(y_logits, nbaLabelTrain)
+    acc = accuracy_fn(y_true=nbaLabelTrain,y_pred=y_pred)
+
+    optimizer.zero_grad()
+
+    loss.backward()
+
+    optimizer.step()
+
+    model_0.eval()
+    with torch.inference_mode():
+        # 1. Forward Pass
+        test_logits = model_0(nbaStatLineTest).squeeze()
+        test_pred = torch.round(torch.sigmoid(test_logits))
+        
+        # 2. Calculate loss and accuracy
+        test_loss = loss_fn(test_logits,nbaLabelTest)
+        test_acc = accuracy_fn(y_true=nbaLabelTest,y_pred=test_pred)
+
+    if epoch % 200 == 0:
+        print(y_pred)
+        print(nbaLabelTrain)
+        print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test Accuracy: {test_acc:.2f}%")
+
+
+model_0.eval()
+with torch.inference_mode():
+    y_preds = torch.round(torch.sigmoid(model_0(nbaStatLineTest).squeeze()))
+
+y_preds, nbaLabelTest
+
+#It is working but it is underfitted because of the lack of data!!!
